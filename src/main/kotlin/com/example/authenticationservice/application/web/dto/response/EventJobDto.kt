28d770fcca063fe32188a07d7959bd5a7a60ea8d
patrik-rangel <rangelsouza17@gmail.com>
2023-10-15 20:29:51 -0300

    Changing object EventJobDto

--- src/main/kotlin/com/example/authenticationservice/application/web/dto/response/EventJobDto.kt
+++ src/main/kotlin/com/example/authenticationservice/application/web/dto/response/EventJobDto.kt
@@ -3,25 +3,28 @@ package com.example.authenticationservice.application.web.dto.response
 import com.example.authenticationservice.domain.entities.EventJob
 
 data class EventJobDto (
-        val id: Long,
-        val fkEvento : Long,
-        val instrumentName : String,
-        val payment : Double?,
-        val isAvailable: Boolean?
+    val id: Long,
+    val fkEvento : Long,
+    val instrumentId : Long,
+    val instrumentName: String,
+    val payment : Double?,
+    val isAvailable: Boolean?,
 ) {
     constructor(eventJob: EventJob): this(
             id = eventJob.id,
             fkEvento = eventJob.event.id,
-            instrumentName = eventJob.instrument.name,
+            instrumentId = eventJob.instrument.id,
             payment = eventJob.payment,
-            isAvailable = eventJob.musician == null
+            isAvailable = eventJob.musician == null,
+            instrumentName = eventJob.instrument.name
     )
 
         constructor(): this(
                 id = 0,
                 fkEvento = 0,
-                instrumentName = "",
+                instrumentId = 0,
                 payment = 0.0,
-                isAvailable = false
+                isAvailable = false,
+                instrumentName = ""
         )
 }
